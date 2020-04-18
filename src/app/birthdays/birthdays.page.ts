801053178bfae22c8be53d86b7dc410a8977ede3
LucasPanetto <lucaspanetto@ucl.br>
2020-04-17 22:56:57 -0300

    Removendo paginas geradas automaticamente, implementando toolbar, segment e text

---
+++ src/app/birthdays/birthdays.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-birthdays',
+  templateUrl: './birthdays.page.html',
+  styleUrls: ['./birthdays.page.scss'],
+})
+export class BirthdaysPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
